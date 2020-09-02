7b4759f3fc80a9f32b630c59e3df34a1d333e9d1
Yato S <yatoneco0015@gmail.com>
2020-09-02 15:55:27 +0900

    make it better

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = os.environ["DISCORD_BOT_TOKEN"]
+TOKEN = "NzM0MDExNjY2Nzg1ODk0NDUx.XxLfog.SO0v45hGVMakZCPiMSGoWOkGin4"
 voice = None
 
 # 接続に必要なオブジェクトを生成
@@ -13,6 +13,8 @@ client = discord.Client()
 url = "https://youtu.be/nnBw3H7OBu4"
 kaomozi_1 = "（´・ω・`）.;:…（´・ω...:.;::..（´・;::: .:.;: ｻﾗｻﾗ.."
 botchannel = 637253650258984967
+channelmusic_id = 740892276154171402
+
 
 
 askii_art1 = "\n  _   _                ____        _   \n | \ | |              |  _ \      | |  \n |  \| | ___  ___ ___ | |_) | ___ | |_ \n | . ` |/ _ \/ __/ _ \|  _ < / _ \| __|\n | |\  |  __| (_| (_) | |_) | (_) | |_ \n |_| \_|\___|\___\___/|____/ \___/ \__|\n"
@@ -48,37 +50,67 @@ async def on_message(message):
             await client.logout()
             await sys.exit()
         else:
-            await message.channel.send(あたに権限がありません)
+            await message.channel.send("あたに権限がありません")
 
     if message.content.startswith("https://youtu.be/"):
-        channel = [message.id for message in message.author.voice.channel.members]
-        if (client.user.id in channel):
-            channel_txt = client.get_channel(637253650258984967)
-            message.author = client.get_user(348386897795612672)
-            await channel_txt.send(";;p " + message.content)
+        if message.channel.id == channelmusic_id:
+            channel = [message.id for message in message.author.voice.channel.members]
+            if (client.user.id in channel):
+                channel_txt = client.get_channel(637253650258984967)
+                message.author = client.get_user(348386897795612672)
+                await channel_txt.send(";;p " + message.content)
+            else:
+                voice_state = message.author.voice
+                channel = voice_state.channel
+                await channel.connect()
+                print("connected to:",channel.name)
+                channel_txt = client.get_channel(637253650258984967)
+                message.author = client.get_user(348386897795612672)
+                await channel_txt.send(";;p " + message.content)
         else:
-            voice_state = message.author.voice
-            channel = voice_state.channel
-            await channel.connect()
-            print("connected to:",channel.name)
-            channel_txt = client.get_channel(637253650258984967)
-            message.author = client.get_user(348386897795612672)
-            await channel_txt.send(";;p " + message.content)
+            channelm = client.get_channel(740892276154171402)
+            embed = discord.Embed(description="音楽のリクエストは" + channelm.mention + "ここへ", color=0x2e3cff)
+            await message.channel.send(embed=embed)
 
     if message.content.startswith("https://www.youtube.com/"):
-        channel = [message.id for message in message.author.voice.channel.members]
-        if (client.user.id in channel):
-            channel_txt = client.get_channel(637253650258984967)
-            message.author = client.get_user(348386897795612672)
-            await channel_txt.send(";;p " + message.content)
+        if message.channel.id == channelmusic_id:
+            channel = [message.id for message in message.author.voice.channel.members]
+            if (client.user.id in channel):
+                channel_txt = client.get_channel(637253650258984967)
+                message.author = client.get_user(348386897795612672)
+                await channel_txt.send(";;p " + message.content)
+            else:
+                voice_state = message.author.voice
+                channel = voice_state.channel
+                await channel.connect()
+                print("connected to:",channel.name)
+                channel_txt = client.get_channel(637253650258984967)
+                message.author = client.get_user(348386897795612672)
+                await channel_txt.send(";;p " + message.content)
         else:
-            voice_state = message.author.voice
-            channel = voice_state.channel
-            await channel.connect()
-            print("connected to:",channel.name)
-            channel_txt = client.get_channel(637253650258984967)
-            message.author = client.get_user(348386897795612672)
-            await channel_txt.send(";;p " + message.content)
+            channelm = client.get_channel(740892276154171402)
+            embed = discord.Embed(description="音楽のリクエストは" + channelm.mention + "ここへ", color=0x2e3cff)
+            await message.channel.send(embed=embed)
+
+    if message.content.startswith("https://m.youtube.com/"):
+        if message.channel.id == channelmusic_id:
+            channel = [message.id for message in message.author.voice.channel.members]
+            if (client.user.id in channel):
+                channel_txt = client.get_channel(637253650258984967)
+                message.author = client.get_user(348386897795612672)
+                await channel_txt.send(";;p " + message.content)
+            else:
+                voice_state = message.author.voice
+                channel = voice_state.channel
+                await channel.connect()
+                print("connected to:",channel.name)
+                channel_txt = client.get_channel(637253650258984967)
+                message.author = client.get_user(348386897795612672)
+                await channel_txt.send(";;p " + message.content)
+        else:
+            channelm = client.get_channel(740892276154171402)
+            embed = discord.Embed(description="音楽のリクエストは" + channelm.mention + "ここへ", color=0x2e3cff)
+            await message.channel.send(embed=embed)
 
     if message.content == ("join") or message.content == ("jn"):
         voice_state = message.author.voice
